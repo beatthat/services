a1245cd6a9f885340d89d74aef5f7e6659653f64
Larry Kirschner <larrykirschner@gmail.com>
2018-06-06 23:18:08 -0700

    improved counter example, still work in progress

--- Samples/CounterService.cs
+++ Samples/CounterService.cs
@@ -5,18 +5,27 @@ namespace BeatThat.Service.Examples
     public interface ICounter
     { 
         int count { get; }
+
+        void Increment();
     }
 
     [RegisterService(typeof(ICounter))]
     public class Counter 
-        : MonoBehaviour // services are not required to be monobehaviours 
-                        //  but it's convenient to be able to see them in the unity editor
+        : MonoBehaviour // Services are not required to be monobehaviours 
+                        // but it's convenient to be able to see them in the unity editor
    
-        , ICounter // it's good practice to have narrow interface(s) for services and access them via interface
+        , ICounter      // It's good practice to have narrow interface(s) 
+                        // for services and access them via interface
     {
         public int count
         {
             get; set;
+        }
+
+        public void Increment()
+        {
+            this.count++;
+            Debug.Log("incremented count to " + this.count);
         }
     }
 }
---
+++ Samples/IncrementButton_Injected.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeatThat.Service.Examples
+{
+    [RequireComponent(typeof(Button))]
+    public class IncrementButton_Injected : MonoBehaviour
+    {
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(this.OnClick);
+            DependencyInjection.InjectDependencies(this);
+        }
+
+        public void OnClick()
+        {
+            this.counter.Increment();
+        }
+
+        [Inject] ICounter counter;
+
+    }
+}
